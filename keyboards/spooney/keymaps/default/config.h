6ccd3a2815ec0dc6e5a238dee9597a0bd5297d82
Ian Howson <ian@mutexlabs.com>
2020-05-18 08:20:09 -0700

    Snapshot 200518

---
+++ keyboards/spooney/keymaps/default/config.h
@@ -0,0 +1,67 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+// FIXME: this could be enabled; you haven't soldered it onto your left half yet
+#define SSD1306OLED
+
+#define USE_SERIAL_PD2
+
+#define FORCE_NKRO
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+// #define RGBLED_NUM 27
+#define RGBLED_NUM 4
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
+
+#define RGBLIGHT_LAYERS
+
+// from https://www.reddit.com/r/olkb/comments/72u8ou/qmk_mouse_keys_rock/
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
+
+
+
+// Underglow
+/*
+#undef RGBLED_NUM
+#define RGBLED_NUM 14    // Number of LEDs
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_SLEEP
+*/
---
+++ keyboards/spooney/keymaps/default/keymap.c
@@ -0,0 +1,252 @@
+#include QMK_KEYBOARD_H
+
+#ifdef PROTOCOL_LUFA
+  #include "lufa.h"
+  #include "split_util.h"
+#endif
+#ifdef SSD1306OLED
+  #include "ssd1306.h"
+#endif
+
+
+
+/*
+#ifdef RGBLIGHT_ENABLE
+//Following line allows macro to read current RGB settings
+extern rgblight_config_t rgblight_config;
+#endif
+*/
+
+extern uint8_t is_master;
+
+// enum layer_names {
+#define _QWERTY 0
+// #define _LOWER 1
+#define _RAISE 2
+// #define _ADJUST 3
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  RAISE,
+  // LOWER,
+  // ADJUST,
+};
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* QWERTY
+// TODO: diagram doesn't match keymap
+ * ,------------------------------------------------.             ,-----------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |             |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------+------'         ,---+------+------+------+------+------+------|
+ * |   Tab   |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  [   |  ]   |  \   |  END   |
+ * |---------+-+-----+------+------+------+-------.                 |------+------+------+------+------+------|
+ * |  Esc      |   A  |   S  |   D  |   F  |   G  |            |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |-----------+-+-----+------+------+------+--------.   |        [   |    |    ]  |------+------+------+------+------+------|
+ * |   LShift    |   Z  |   X  |   C  |   V  |   B   |               -------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |-----------+-------------------------------------|    /          /     \      \-----------------------------------------'
+ * |   LCtrl   | RAISE |   LAlt   |   LGUI   | LUTIL |      /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ * `-------------------------------------------------'           '------''--------------------'
+ */
+
+[_QWERTY] = {
+  // LEFT HALF
+  { KC_GRAVE, KC_1,       KC_2,      KC_3,     KC_4,   KC_5,               KC_6, _______},
+  {   KC_TAB, KC_Q,       KC_W,      KC_E,     KC_R,   KC_T,              KC_NO, _______},
+  {   KC_ESC, KC_A,       KC_S,      KC_D,     KC_F,   KC_G,              KC_NO, _______},
+  {  KC_LSFT, KC_NO,      KC_Z,      KC_X,     KC_C,   KC_V,               KC_B, _______},
+  { KC_LCTRL, MO(_RAISE), KC_LALT,   KC_NO, KC_LGUI,   KC_NO, SGUI(KC_LBRACKET), _______},
+  {  _______,    _______, _______, _______, _______, _______,           _______, _______},
+//   { KC_LCTRL, RGB_MODE_FORWARD, KC_LALT, KC_NO,  KC_LGUI, KC_NO, SGUI(KC_LBRACKET)},
+
+  // RIGHT HALF
+  // FIXME: put the right keymap here
+  // FIXME: you changed the row/col count on right side
+  // FIXME: this needs more columns, but you might just use the spare line on the left and use the same row/col mapping on both
+  {           _______, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_AUDIO_MUTE,    KC_END,   KC_PGDOWN,     _______, _______},
+  {              KC_7,              KC_8,            KC_9,          KC_0,  KC_MINUS,    KC_EQUAL,   KC_BSPACE, KC_HOME},
+  {              KC_Y,              KC_U,            KC_I,          KC_O,      KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSLASH},
+  {              KC_H,              KC_J,            KC_K,          KC_L, KC_SCOLON,    KC_QUOTE,    KC_ENTER, KC_PGUP},
+  {           _______,              KC_N,            KC_M,      KC_COMMA,    KC_DOT,    KC_SLASH,   KC_RSHIFT, KC_UP},  // TODO: first col here is FLIP
+  { SGUI(KC_RBRACKET),          KC_SPACE,         KC_RGUI,       KC_RALT,  KC_RCTRL,     KC_LEFT,     KC_DOWN, KC_RIGHT},
+},
+#if 0
+/* LOWER
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+[_LOWER] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
+  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+                             _______, _______, _______, _______, _______,  _______, _______, _______\
+),
+#endif
+/* RAISE
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|      | Left | Down |  Up  |Right |      |
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+ * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+[_RAISE] = {
+    // LEFT HAND
+    {   RESET,   KC_F1,      KC_F2,            KC_F3,       KC_F4,   KC_F5,             KC_F6, _______},
+    { _______, _______,    _______,         KC_MS_UP,     _______, _______,           _______, _______},
+    { _______, _______, KC_MS_LEFT,       KC_MS_DOWN, KC_MS_RIGHT, RGB_HUI,           _______, _______},
+    { _______, _______,    RGB_TOG, RGB_MODE_FORWARD,     RGB_HUI, RGB_SAI,           RGB_VAI, _______},
+    { _______, _______, KC_MS_BTN2,          _______,  KC_MS_BTN1, _______, SGUI(KC_RBRACKET), _______},
+    { _______, _______,    _______,          _______,     _______, _______,           _______, _______},
+
+    // RIGHT HAND
+    { _______, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, _______, _______,   _______, _______},
+    {   KC_F7,               KC_F8,               KC_F9,              KC_F10,  KC_F11,  KC_F12, KC_DELETE, _______},
+    { _______,             _______,               KC_UP,             _______, _______, _______,   _______, _______},
+    { KC_LEFT,             KC_DOWN,               KC_UP,            KC_RIGHT, _______, _______,   _______, _______},
+    { _______,             _______,             _______,             _______, _______, _______,   _______, _______},
+    { _______,             _______,             _______,             _______, _______, _______,   _______, _______},
+},
+
+#if 0
+/* ADJUST
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+  [_ADJUST] = LAYOUT( \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,\
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
+  )
+#endif
+};
+
+int RGB_current_mode;
+
+void matrix_init_user(void) {
+  /*
+    #ifdef RGBLIGHT_ENABLE
+        RGB_current_mode = rgblight_config.mode;
+        rgblight_enable(); // Ian: switch on RGB
+    #endif
+    */
+    //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
+    #ifdef SSD1306OLED
+        iota_gfx_init(!has_usb());   // turns on the display
+    #endif
+}
+
+//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
+#ifdef SSD1306OLED
+
+// When add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+void matrix_scan_user(void) {
+   iota_gfx_task();
+}
+
+void matrix_render_user(struct CharacterMatrix *matrix) {
+  if (is_master) {
+    // If you want to change the display of OLED, you need to change here
+    matrix_write_ln(matrix, read_layer_state());
+    matrix_write_ln(matrix, read_keylog());
+    matrix_write_ln(matrix, read_keylogs());
+    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+    //matrix_write_ln(matrix, read_host_led_state());
+    //matrix_write_ln(matrix, read_timelog());
+  } else {
+    matrix_write(matrix, read_logo());
+  }
+}
+
+void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
+  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
+    memcpy(dest->display, source->display, sizeof(dest->display));
+    dest->dirty = true;
+  }
+}
+
+void iota_gfx_task_user(void) {
+  struct CharacterMatrix matrix;
+  matrix_clear(&matrix);
+  matrix_render_user(&matrix);
+  matrix_update(&display, &matrix);
+}
+#endif//SSD1306OLED
+
+
+
+
+/*
+not working
+const rgblight_segment_t PROGMEM my_raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLED_NUM, HSV_RED}       // Light everything red
+);
+
+const rgblight_segment_t PROGMEM my_game_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLED_NUM, HSV_GREEN}
+);
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_raise_layer,
+    my_game_layer
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Both layers will light up if both kb layers are active
+    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
+    rgblight_set_layer_state(2, layer_state_cmp(state, 2));
+    return state;
+}
+
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
+}
+*/
